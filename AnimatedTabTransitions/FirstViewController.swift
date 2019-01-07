795f67f8d7c92580c684d3c6f95106f39213f6c8
Christopher Casey <Chris@christophers-macbook-pro-6.local>
2019-01-07 12:43:23 -0600

    Initial Commit

---
+++ AnimatedTabTransitions/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AnimatedTabTransitions
+//
+//  Created by Christopher Casey on 1/7/19.
+//  Copyright © 2019 Christopher Casey. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AnimatedTabTransitions/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AnimatedTabTransitions
+//
+//  Created by Christopher Casey on 1/7/19.
+//  Copyright © 2019 Christopher Casey. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
